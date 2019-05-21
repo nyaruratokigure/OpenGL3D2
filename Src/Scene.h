08c9896841b16995cc4660c088ac4e515b72b588
nyaruratokigure <satokigure@yahoo.co.jp>
2019-05-22 02:12:31 +0900

    スタックを使ったシーン移行を実装

--- Src/Scene.h
+++ Src/Scene.h
@@ -5,6 +5,7 @@
 #define SCENE_H_INCLUDED
 #include<memory>
 #include<string>
+#include<vector>
 
 class SceneStack;
 
@@ -39,5 +40,33 @@ private:
 	bool isActive = true;
 };
 using ScenePtr = std::shared_ptr<Scene>;
+
+/*
+シーン管理クラス
+*/
+class SceneStack
+{
+public:
+	static SceneStack& Instance();
+
+	void Push(ScenePtr);
+	void Pop();
+	void Replace(ScenePtr);
+	Scene& Current();
+	const Scene& Current() const;
+	size_t Size() const;
+	bool Empty() const;
+
+	void Update(float);
+	void Render();
+
+private:
+	SceneStack();
+	SceneStack(const SceneStack&) = delete;
+	SceneStack& operator=(const SceneStack&) = delete;
+	~SceneStack() = default;
+
+	std::vector<ScenePtr>stack;
+};
 
 #endif//SCENE_H_INCLUDED
