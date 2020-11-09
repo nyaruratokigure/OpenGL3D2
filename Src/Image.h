046f0f5cf17cc16043322c77e3e5af9da1740c64
聡希 木村 <satokigure@yahoo.co.jp>
2020-11-09 20:39:27 +0900

    D->共有用

---
+++ Src/Image.h
@@ -0,0 +1,13 @@
+/*
+@file Image.h
+*/
+#ifndef Image_H_INCLUDED
+#define Image_H_INCLUDED
+
+class Image
+{
+	void set_image(int no, float x, float y);
+	void reset_image(int no);
+
+}
+#endif // ENEMYACTOR_H_INCLUDED
